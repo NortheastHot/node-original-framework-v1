5a6ba13feb7dbe251a401ee9f4def502a19031ad
qiheng <aiguozhex@163.com>
2021-04-11 11:11:22 +0800

    静态路由模拟

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
 const serverHandle = (req,res)=>{
     // 设置返回格式JSON
     res.setHeader('Content-type', 'application/json')
-    const resData = {
-        name:'kizi',
-        site: 223,
-        env: process.env.NODE_ENV // 获取环境
+
+    // 处理Blog路由
+    const blogData = handleBlogRouter(req,res)
+    if(blogData){
+        res.end(JSON.stringify(blogData))
+        return
     }
-    res.end(JSON.stringify(resData))
+    // 处理User路由
+    const userData = handleUserRouter(req,res)
+    if(userData){
+        res.end(JSON.stringify(userData))
+        return
+    }
+    // 404
+    res.writeHead(404,{"Content-type":'text/plain'})
+    res.write("404 Not Found")
+    res.end()
 }
 
 module.exports = serverHandle 
