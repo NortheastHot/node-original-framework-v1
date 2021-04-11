b1f8f8113606f32deb7bbed7bd8dc68ad0a54e11
qiheng <aiguozhex@163.com>
2021-04-11 10:27:48 +0800

    使用nodemon、cross-env包

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const serverHandle = (req,res)=>{
     res.setHeader('Content-type', 'application/json')
     const resData = {
         name:'kizi',
-        site: 223
+        site: 223,
+        env: process.env.NODE_ENV // 获取环境
     }
     res.end(JSON.stringify(resData))
 }
 
-module.exports = serverHandle
+module.exports = serverHandle 
